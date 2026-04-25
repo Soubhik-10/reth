f0ff853228ee1da1920a9ede4ca32121baacb3b1
Ishika Choudhury <117741714+Rimeeeeee@users.noreply.github.com>
2026-04-25 12:22:26 +0530

    tracing

--- crates/rpc/rpc-server-types/src/result.rs
+++ crates/rpc/rpc-server-types/src/result.rs
@@ -142,9 +142,12 @@ pub fn rpc_err(
     msg: impl Into<String>,
     data: Option<&[u8]>,
 ) -> jsonrpsee_types::error::ErrorObject<'static> {
+    let msg = msg.into();
+    tracing::info!("RPC error (code: {code}): {}", msg.clone());
+    tracing::info!(?data, "RPC error data");
     jsonrpsee_types::error::ErrorObject::owned(
         code,
-        msg.into(),
+        msg,
         data.map(|data| {
             jsonrpsee_core::to_json_raw_value(&alloy_primitives::hex::encode_prefixed(data))
                 .expect("serializing String can't fail")
